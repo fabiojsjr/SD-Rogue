3a13e6ab90f98bdfe457222bf750f9f4c366a268
Evan Budwill <Evan.Budwill@yahoo.ca>
2026-04-16 15:18:28 -0300

    fixed spelling mistake

--- RogueLib/enemies/Merchant.cs
+++ RogueLib/enemies/Merchant.cs
@@ -2,13 +2,13 @@
 
 namespace RogueLib.enemies
 {
-    internal class Merchent
+    internal class Merchant
     {
         private Vector2 pos;
         private string v1;
         private int v2;
 
-        public Merchent(Vector2 pos, string v1, int v2)
+        public Merchant(Vector2 pos, string v1, int v2)
         {
             this.pos = pos;
             this.v1 = v1;
